1ad933c9150ba84d294d16ab28f4054bfd5cafb9
Kozo Komiya <kozo.komiya@gmail.com>
2020-03-23 18:06:18 +0900

    first release

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tab-menu></tab-menu>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabMenu from './components/TabMenu'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TabMenu
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+ outputDir: 'docs',   // change output dir for github pages
+}
